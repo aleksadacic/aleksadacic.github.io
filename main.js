a1134d2d4e5be0b481e622efe4c40dd0e8193503
aleksadacic <aleksa.dacic99@gmail.com>
2021-02-23 09:58:23 +0100

    okok

---
+++ main.js
@@ -0,0 +1,7 @@
+function homeClick() {
+    document.body.style.backgroundImage = 'url("backgrnd.png")';
+}
+
+function otherClick() {
+    document.body.style.backgroundImage = 'url("bg2.png")';
+}
