8ea9b28ea8906edfc590e4f0354e0242c5b779c6
aleksadacic <aleksa.dacic99@gmail.com>
2021-05-12 00:41:47 +0200

    deisng

--- main.js
+++
@@ -1,7 +0,0 @@
-function homeClick() {
-    document.body.style.backgroundImage = 'url("backgrnd.png")';
-}
-
-function otherClick() {
-    document.body.style.backgroundImage = 'url("bg2.png")';
-}
